9aa0d612226c38e4f721c6b89abd1fe374319939
jtreeves <jackson.t.reeves@gmail.com>
2020-12-16 10:58:21 -0500

    [nav] Create navigation links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Show from './pages/Show'
 import New from './pages/New'
 import Edit from './pages/Edit'
+import Nav from './components/Nav'
 
 import './App.css'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className='App'>
+                <Nav />
                 <h1>App!</h1>
-                <Home />
-                <Show />
-                <New />
-                <Edit />
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route path='/show/:id'>
+                    <Show />
+                </Route>
+                <Route path='/new'>
+                    <New />
+                </Route>
+                <Route path='/edit/:id'>
+                    <Edit />
+                </Route>
             </div>
         )
     }
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/new'>New</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
