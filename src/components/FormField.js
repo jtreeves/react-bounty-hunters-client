af7c6d321f4e974a4d81cad2cfbb51bdbfe8a0e5
jtreeves <jackson.t.reeves@gmail.com>
2020-12-16 14:56:53 -0500

    [New] Set state based on user input

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,10 +1,8 @@
 function FormField(props) {
     return (
         <form>
-            <label htmlFor='name'>Name: </label>
-            <input name='name' value={this.state.name} onChange={(e) => {
-                this.setState({name: e.target.value})
-            }}/>
+            <label htmlFor={props.labelLink}>{props.displayLabel}: </label>
+            <input name={props.labelLink} value={props.value} onChange={props.handler}/>
         </form>
     )
 }
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,20 +1,40 @@
 import { Component } from 'react'
 
+import FormField from '../components/FormField'
+
 class New extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: ''
+            name: '',
+            wantedFor: ''
         }
     }
     
+    setName = (e) => {
+        this.setState({name: e.target.value})
+    }
+
+    setWantedFor = (e) => {
+        this.setState({wantedFor: e.target.value})
+    }
+
     render() {
         return (
             <div>
                 <h1>Create a new bounty</h1>
-                <div>
-                    
-                </div>
+                <FormField
+                    labelLink='name'
+                    displayLabel='Name'
+                    value={this.state.name}
+                    handler={this.setName}
+                />
+                <FormField
+                    labelLink='wantedFor'
+                    displayLabel='Wanted For'
+                    value={this.state.wantedFor}
+                    handler={this.setWantedFor}
+                />
             </div>
         )
     }
