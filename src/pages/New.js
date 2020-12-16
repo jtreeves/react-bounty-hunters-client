a20419b9652df26718f3b668f8d619188ebd8ee6
jtreeves <jackson.t.reeves@gmail.com>
2020-12-16 14:46:15 -0500

    [New] Add form for name

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,25 @@
 import { Component } from 'react'
 
 class New extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: ''
+        }
+    }
+    
     render() {
         return (
             <div>
-                New
+                <h1>Create a new bounty</h1>
+                <div>
+                    <form>
+                        <label htmlFor='name'>Name: </label>
+                        <input name='name' value={this.state.name} onChange={(e) => {
+                            this.setState({name: e.target.value})
+                        }}/>
+                    </form>
+                </div>
             </div>
         )
     }
